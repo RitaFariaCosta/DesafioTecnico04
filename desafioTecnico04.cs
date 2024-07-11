1ebfa9f064e521f5dea8d77d204c962163e5a921
RitaFariaCosta <alicecooperbloosom@gmail.com>
2024-07-11 15:04:09 -0300

    DesafioTecnico04
    DesafioTecnico04

--- desafioTecnico04.cs
+++ desafioTecnico04.cs
@@ -11,8 +11,9 @@ class HelloWorld {
       Amigos[] amigos = new Amigos[3];
       
       for(int i = 0; i < 3; i++){
-        Console.WriteLine($"\nInforme os dados do amigo(a) {i + 1}");
-         
+          
+        Console.WriteLine($"\nInforme os dados do amigo(a) {i + 1} de Inej Ghafa");
+        
         Console.Write("Digite o nome do amigo(a): ");
         amigos[i].Nome = Console.ReadLine();
         Console.Write("Digite a idade do amigo(a): ");
@@ -21,9 +22,11 @@ class HelloWorld {
         amigos[i].Qualidade = Console.ReadLine();
     }
     
+    Console.WriteLine("\nOs amigos de Inej Ghafa são: ");
+    
     for (int i = 0; i < 3; i++)
         {
-            Console.WriteLine($"\nAmigo(a) {i + 1}: {amigos[i].Nome}, {amigos[i].Idade}, {amigos[i].Qualidade}");
+            Console.WriteLine($"Amigo(a) {i + 1}: Nome:{amigos[i].Nome}, Idade: {amigos[i].Idade}, Qualidades: {amigos[i].Qualidade}");
         }
   }
 }
